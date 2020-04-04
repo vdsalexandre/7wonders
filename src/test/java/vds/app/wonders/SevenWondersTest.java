28c9016a49a5fae2824b74c6942fbc011a42775d
vdsalex <alexandre.vandensteen@gmail.com>
2020-04-04 17:32:23 +0200

    first green test -> env setted ok

---
+++ src/test/java/vds/app/wonders/SevenWondersTest.java
@@ -0,0 +1,13 @@
+package vds.app.wonders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SevenWondersTest {
+
+    @Test
+    void settingsTest() {
+        assertThat(true).isTrue();
+    }
+}
